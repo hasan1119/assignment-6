e07dd39282de34b2216c90c11c465d111f92a6ac
Md. Rokibul Hasan <hasan.webappsdeveloper@gmail.com>
2021-09-02 08:23:51 +0600

    second

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
   const bookContainer = document.getElementById('book-container')
-  var bookDetails = document.getElementById('book-details')
+  var result = document.getElementById('result')
   document.getElementById('search-btn').addEventListener('click', () => {
       bookContainer.innerHTML = `
     <div class="w-100 h-100 d-flex justify-content-center align-items-center">
@@ -10,7 +10,8 @@
     `
       const searchText = document.getElementById('search-text');
       if (searchText.value == '') {
-          bookContainer.innerText = ("Please with valid book name:")
+          bookContainer.innerHTML = ''
+          result.innerHTML = "Please search with a valid book name"
       } else {
           const api = `http://openlibrary.org/search.json?q=${searchText.value}`
           searchText.value = ''
@@ -24,14 +25,15 @@
   const showData = (books) => {
       var newArr = books.filter(one => one.cover_i !== undefined && one.author_name !== undefined && one.publisher !== undefined && one.title !== undefined && one.first_publish_year !== undefined)
       if (newArr.length === 0) {
-          bookDetails.innerHTML = ''
-          bookContainer.innerHTML = 'NO Resut Found!'
+          result.innerHTML = ''
+          result.innerHTML = 'No Result Found!'
+          bookContainer.innerHTML = ''
       } else {
 
           var newP = document.createElement('p')
           newP.innerHTML = `You got ${newArr.length} books`
-          bookDetails.innerHTML = ''
-          bookDetails.appendChild(newP)
+          result.innerHTML = ''
+          result.appendChild(newP)
 
           bookContainer.innerHTML = ''
           newArr.forEach(book => {
@@ -41,8 +43,8 @@
               const newDiv = document.createElement('div')
               newDiv.innerHTML = `
 
-
-      <div class="card my-3" style="width: 18rem;height:auto"> 
+    <div class="card-group">
+      <div class="card my-4" style="width: 20rem;min-height:450px"> 
       <img class="card-img-top img-fluid" style="width:100%; height:250px" src='https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg'>
       <div class="card-body">
       <h5 class="card-title text-primary">${book.title}</h5>
@@ -50,6 +52,7 @@
       <p class="card-text text-warning">${book.publisher[0]}</p>
       <p class="card-text">First Publish Year: ${book.first_publish_year}</p>
      
+      </div>
       </div>
       </div>`
 
